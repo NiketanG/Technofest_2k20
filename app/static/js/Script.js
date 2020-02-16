b255ca692099ef9f3622e61fee6a99cebf59c6cd
NiketanG <nikegulekar@gmail.com>
2020-02-16 21:15:27 +0530

    Fixed countdown date

--- app/static/js/Script.js
+++ app/static/js/Script.js
@@ -90,7 +90,7 @@ function toggle() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 28, 2020 09:30:00").getTime();
+var countDownDate = new Date("Mar 6, 2020 09:30:00").getTime();
 // Get today's date and time
 var now = new Date().getTime();
 
